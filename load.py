0ba9eea0206fff802aac6b40c363b4a7ae5bc44d
lilokhang <lilokhang007@gmail.com>
2021-05-26 02:13:30 +0800

    Preprocessing code for budget data

--- load.py
+++ load.py
@@ -18,6 +18,17 @@ df_acc_bal = df_acc['balance_in_HKD'].\
     apply(pd.Series)
 df_acc_bal.columns = ['bal_sum']
 
+# extract budget data
+df_bud['saved_amount'] = df_bud.apply(lambda row: convert_to_HKD(
+    row, convert_field='saved_amount', from_currency_field='currency'), axis=1)
+df_bud = df_bud[df_bud['status'] != 'Failed'] # eliminate failed budgets
+
+df_bud_amt = df_bud['saved_amount'].\
+    groupby(df_bud['psid']).unique().\
+    apply(lambda ls: sum(ls)).\
+    apply(pd.Series)
+df_bud_amt.columns = ['saved_amount_sum']
+
 # extract transaction data and convert currencies
 df_tra['amount_in_HKD'] = df_tra.apply(lambda row: convert_to_HKD(row), axis=1)
 df_tra = df_tra[['psid', 'amount_in_HKD', 'areix_category']]
@@ -25,8 +36,15 @@ df_tra = df_tra[['psid', 'amount_in_HKD', 'areix_category']]
 # distribute 'amount_in_HKD' column by areix_category, using a pivot table
 df_tra_pivot = pd.pivot_table(df_tra, index='psid', columns='areix_category', values='amount_in_HKD')
 df_tra_pivot = df_tra_pivot.join(df_acc_bal, on='psid')
+df_tra_pivot = df_tra_pivot.join(df_bud_amt, on='psid')
 df_tra_pivot = df_tra_pivot.fillna(0) # fill all nan with 0
-df_tra_pivot.to_csv(preprocessed_dir + 'Data_processed.csv', index=False)
+
+# sort the index correctly
+sorted_index = sorted(df_tra_pivot.index, key=lambda x: int(str(x).split('_')[1]))
+df_tra_pivot = df_tra_pivot.reindex(sorted_index)
+
+# output the file as a processed csv
+df_tra_pivot.to_csv(preprocessed_dir + 'Data_processed.csv')
 
 
 
